233d427a53eac03dcceeb3ff3f4339516850cd9d
Steven Hoelscher <stevenho@zillowgroup.com>
2017-10-31 09:25:11 -0700

    Build Python 2.x versions with wide encode

--- build_tools/travis/build_wheels.sh
+++ build_tools/travis/build_wheels.sh
@@ -2,6 +2,46 @@
 
 set -e -x
 
+
+function lex_pyver {
+    # Echoes Python version string padded with zeros
+    # Thus:
+    # 3.2.1 -> 003002001
+    # 3     -> 003000000
+    echo $1 | awk -F "." '{printf "%03d%03d%03d", $1, $2, $3}'
+}
+
+function build_wheel {
+    local pyver=$1
+    local arch=$2
+    local ucs_setting=$3
+
+    # https://www.python.org/dev/peps/pep-0513/#ucs-2-vs-ucs-4-builds
+    ucs_tag="m"
+    if [ "$ucs_setting" = "ucs4" ]; then
+        ucs_tag="${ucs_tag}u"
+    fi
+
+    ML_PYTHON_VERSION=$(python3 -c \
+        "print('cp{maj}{min}-cp{maj}{min}{ucs}'.format( \
+               maj='${pyver}'.split('.')[0], \
+	       min='${pyver}'.split('.')[1], \
+	       ucs='${ucs_tag}'))")
+
+    ML_IMAGE="quay.io/pypa/manylinux1_${arch}"
+    docker pull "${ML_IMAGE}"
+    docker run \
+        --name "${DOCKER_CONTAINER_NAME}" \
+        -v "${_root}":/io \
+        -e "PYMODULE=${PYMODULE}" \
+        -e "PYTHON_VERSION=${ML_PYTHON_VERSION}" \
+        "${ML_IMAGE}" "/io/build_tools/travis/build_manywheels_linux.sh"
+    sudo docker cp "${DOCKER_CONTAINER_NAME}:/io/dist/" "${_root}/dist/"
+    docker rm $(docker ps -a -f status=exited -q)
+}
+
+
+
 if [[ "${DEPLOY}" != true ]]; then
     # Not a release
     exit 0
@@ -16,25 +56,10 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
     echo "Building LINUX OS wheels"
 
     for pyver in ${PYTHON_VERSION}; do
-        ML_PYTHON_VERSION=$(python3 -c \
-            "print('cp{maj}{min}-cp{maj}{min}m'.format( \
-                   maj='${pyver}'.split('.')[0], \
-                   min='${pyver}'.split('.')[1]))")
-	python3 -c "import sys; opts = {1114111: 'ucs4', 65535: 'ucs2'}; print(opts.get(sys.maxunicode));"
-
-
-        for arch in x86_64; do
-            ML_IMAGE="quay.io/pypa/manylinux1_${arch}"
-            docker pull "${ML_IMAGE}"
-            docker run \
-                --name "${DOCKER_CONTAINER_NAME}" \
-                -v "${_root}":/io \
-                -e "PYMODULE=${PYMODULE}" \
-                -e "PYTHON_VERSION=${ML_PYTHON_VERSION}" \
-                "${ML_IMAGE}" "/io/build_tools/travis/build_manywheels_linux.sh"
-            sudo docker cp "${DOCKER_CONTAINER_NAME}:/io/dist/" "${_root}/dist/"
-            docker rm $(docker ps -a -f status=exited -q)
-        done
+        build_wheel $pyver "x86_64" "ucs2"
+        if [ $(lex_pyver $pyver) -lt $(lex_pyver 3.3) ]; then
+	    build_wheel $pyver "x86_64" "ucs4"
+        fi
     done
 elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     # this should be all that's required, right? We already removed the .egg-info
